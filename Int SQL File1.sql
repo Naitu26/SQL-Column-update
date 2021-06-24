9a90857b5e2020dceb9d23fa091c2669e93ec8c9
Naitu26 <86136636+Naitu26@users.noreply.github.com>
2021-06-24 01:16:27 -0700

    Add files via upload

---
+++ Int SQL File1.sql
@@ -0,0 +1,10 @@
+SELECT * FROM project.uci_credit_card;
+use uci_credit_card;
+select *from uci_credit_card;
+update uci_credit_card 
+set SEX = int to str;
+Alter table uci_credit_card 
+alter column SEX 
+varchar(10);
+Alter table uci_credit_card 
+rename column SEX to Gender;
